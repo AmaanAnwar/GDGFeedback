1808f8e9652c2ad00fe94f871218fbc4c451f722
Amaan <amaan.anwar789@gmail.com>
2020-02-19 04:58:28 -0600

    Completed Form

--- app/src/main/java/com/example/gdgapplication/MainActivity.java
+++ app/src/main/java/com/example/gdgapplication/MainActivity.java
@@ -17,66 +17,20 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    Button submitBtn;
-    EditText nameEdt;
-    RatingBar ratingBar;
-    Spinner qualification;
-    RadioButton studentRB,profRB;
-    EditText suggesyionET;
-    SeekBar ageSB;
-    CheckBox agreeSB;
+Button butn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        nameEdt = (EditText) findViewById(R.id.nameedt);
-        submitBtn = (Button)findViewById(R.id.submitbtn);
-        submitBtn.setOnClickListener(new View.OnClickListener(){
-                                         @Override
-                                         public void onClick(View view){
-
-                                             Intent i = new Intent(MainActivity.this,ThankyouActivity.class);
-                                             i.putExtra("name",nameEdt.getText().toString());
-                                             startActivity(i);
-                                         }
-                                     }
-        );
-
-
-        ratingBar = findViewById(R.id.rb);
-        qualification =findViewById(R.id.qualificationspn);
-        studentRB =findViewById(R.id.studentRB);
-        profRB=findViewById(R.id.profRB);
-        suggesyionET =findViewById(R.id.suggest);
-        ageSB=findViewById(R.id.seekbar);
-        agreeSB =findViewById(R.id.consentCB);
-        submitBtn.setOnClickListener(new View.OnClickListener() {
+        butn=findViewById(R.id.butn);
+        butn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name= nameEdt.getText().toString();
-                String suggestion=suggesyionET.getText().toString();
-                String qualificat = qualification.getSelectedItem().toString();
-                String occupation = "Not Selected";
-                if(studentRB.isChecked()){
-                    occupation="Student";
-                }
-                if(profRB.isChecked()){
-                    occupation="Professional";
-                }
-                int age = ageSB.getProgress();
-                boolean isAgree = agreeSB.isChecked();
-                int rating=ratingBar.getProgress();
-                GDGFeedback gdgFeedback= new GDGFeedback(name,occupation,rating,qualificat,age,suggestion,isAgree);
-                Intent intent =new Intent(MainActivity.this,ThankyouActivity.class);
-                intent.putExtra("name",nameEdt.getText().toString());
-                intent.putExtra("feedback",gdgFeedback);
+                Intent intent=new Intent(MainActivity.this,Form.class);
                 startActivity(intent);
             }
         });
     }
-
-
 
 
 }
